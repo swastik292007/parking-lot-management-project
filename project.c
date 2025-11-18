56414abcf417ba7128f3f64075793fced897cae8
Swastik <swastik292007@gmail.com>
2025-11-19 00:12:16 +0530

    calling libraries
    calling libraries

---
+++ project.c
@@ -0,0 +1,7 @@
+//AIM OF THIS PROJECT IS TO CREATE AN AUTOMATED PARKING LOT MANAGEMENT PROGRAM USING C TO MAKE IT EASY TO HANDLE RECORD OF VEHICLES.
+
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+
